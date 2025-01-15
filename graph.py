845c67079499169493f8be33272b14cf20993a3b
BuildTools <unconfigured@null.spigotmc.org>
2025-01-15 11:29:15 -0800

    Initial version of files

--- graph.py
+++ graph.py
@@ -1,14 +1,15 @@
-'''
-Untitled1.acq
+"""
 0.5 msec/sample	Sample rate =	2000
 2 channels
 EMG (5 - 250 Hz w/notch)
 mV
 Clench Force
 kg
-'''
+"""
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+
+df = pd.read_csv('B01_Fatigue.csv')
 
